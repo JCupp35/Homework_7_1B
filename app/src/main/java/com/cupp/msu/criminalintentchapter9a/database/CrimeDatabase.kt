37540d065e3c7928f229cfb23ff97348e08485ac
Justin Cupp <jcupp35@gmail.com>
2024-04-20 00:07:29 -0500

    Chapter 12B Finished

--- app/src/main/java/com/cupp/msu/criminalintentchapter9a/database/CrimeDatabase.kt
+++ app/src/main/java/com/cupp/msu/criminalintentchapter9a/database/CrimeDatabase.kt
@@ -2,12 +2,14 @@ package com.cupp.msu.criminalintentchapter9a.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.cupp.msu.criminalintentchapter9a.Crime
 
-@Database(entities = [Crime:: class], version=1)
+@Database(entities = [Crime:: class], version=1, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase: RoomDatabase() {
     abstract fun crimeDAO(): CrimeDao
+
 
 }
